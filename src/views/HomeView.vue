8ee4fa3dbccf8b06957c03e52f07210092d407d8
Javier <javijsol96@gnail.com>
2024-06-03 00:29:08 -0600

    empezando a crear la navegacio, la pagina home con lo que es la presentación del loggi (aún inconcluso)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br>
+    <div class="row">
+      <div class="row"></div>
+      <div class="col">
+        <img alt="Vue logo" src="../assets/img/Inifap_logos.jpg" />
+      </div>
+      <div class="col"></div>
+    </div>
+  
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
