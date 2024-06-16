59adf63c1bcf2293a25a24b638bfa5e820e482fb
Javier <javijsol96@gnail.com>
2024-06-15 20:19:53 -0600

    integrando jwt v2.4

--- Backend/src/auth/Controllers/loginController.js
+++ Backend/src/auth/Controllers/loginController.js
@@ -1,21 +1,29 @@
 import { PasswordService } from "../Services/PasswordService.js";
 import { User } from "../../Users/Models/User.js";
+import jwt from 'jsonwebtoken'; 
+import { env } from "../../Shared/env.js";
 
 export const loginController = async (request, response) => {
     const { name, password } = request.body;
 
-    const user = await User.findOne(
-        {
-            where: { name: name }
-        });
+    const user = await User.findOne({
+        where: { name: name }
+    });
 
     const userExist = await PasswordService.check(password, user.password);
 
-    console.log(userExist);
-
     if (!userExist) {
-        return "Error";
+        return response.status(401).json({ mesage: 'Los datos ingresados son incorrectos' });
     }
-    return true;
 
+    const token = jwt.sign({ userId: user.id }, env('JWT_SECRET_KEY'), { expiresIn: '1h' });
+
+    const jsonResponse = {
+        message: "¡Usuario logueado correctamente!",
+        data: {
+            token
+        }
+    };  
+
+    return response.json(jsonResponse);
 };
