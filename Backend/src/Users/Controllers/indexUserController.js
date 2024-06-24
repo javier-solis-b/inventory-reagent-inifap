b3e8e3fe9fd30798b8f8a811545b359c2832f30f
Javier <javijsol96@gnail.com>
2024-06-24 14:14:47 -0600

    se implementó la validación del token de ususario v3.2

--- Backend/src/Users/Controllers/indexUserController.js
+++ Backend/src/Users/Controllers/indexUserController.js
@@ -1,5 +1,14 @@
 import { User } from '../Models/User.js';
+import { TokenService } from '../../auth/Services/TokenService.js';
+export const indexUserController = async (request, response) => {
 
-export const indexUserController  = async (request, response) => {
+    const token = request.headers.authorization;
+     
+    if(!TokenService.isValid(token)){
+        return response.status(403).json({
+            message: ' Datos incorrectos :('
+        })
+    }
+
     response.json(await User.findAll());
 }
---
+++ Backend/src/auth/Services/TokenService.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import {env} from '../../Shared/env.js'
+
+export class TokenService{
+
+    static isValid(token){
+        if(!token){
+            return false;
+        }
+        try {
+            const result = jwt.verify(token, env('JWT_SECRET_KEY'));// Usé process.env para acceder a la variable de entorno
+        } catch (error) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
